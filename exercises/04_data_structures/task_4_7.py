a509245c5532192a042efd72cbc205a3107096de
mdan4ikx <mdan4ikx.mdan4ikx@mail.ru>
2020-09-14 17:05:55 +0600

    Did ska!

--- exercises/04_data_structures/task_4_7.py
+++ exercises/04_data_structures/task_4_7.py
@@ -11,12 +11,15 @@
 
 mac = "AAAA:BBBB:CCCC"
 abc = mac.split(':')
-a = int((abc[0]),16)
-b = int((abc[1]),16)
-c = int((abc[-1]),16)
-abc = (bin(a)+bin(b)+bin(c))
-abc = abc.split('0b')
-print(abc[1]+abc[2]+abc[-1])
+#a = int((abc[0]),16)
+#b = int((abc[1]),16)
+#c = int((abc[-1]),16)
+abc = ''.join(abc)
+print(abc) 
+#abc = (bin(a)+bin(b)+bin(c))
+#abc = abc.split('0b')
+#print(abc[1]+abc[2]+abc[-1])
 
-
+abc1 = bin(int(abc,16))
+print(abc1[2:])
 
